2460793df20de9afc4744e54bd6d958d48e8915d
23n <post@toktan.org>
2013-06-11 23:03:18 +0200

    version

--- neptune/theme.php
+++ neptune/theme.php
@@ -4,14 +4,14 @@
  * Name: Neptune - a mobile ~friendica-theme
  * Description: Based on Frost-mobile
  * Credits: Credits to Zach P for creating Frost-mobile! Navigation icons taken from http://iconza.com. Other icons taken from http://thenounproject.com, including: Like, Dislike, Black Lock, Unlock, Pencil, Tag, Camera, Paperclip (Marie Coons), Folder (Sergio Calcara), Chain-link (Andrew Fortnum), Speaker (Harold Kim), Quotes (Henry Ryder), Video Camera (Anas Ramadan), and Left Arrow, Right Arrow, and Delete X (all three P.J. Onori). All under Attribution (CC BY 3.0). Others from The Noun Project are public domain or No Rights Reserved (CC0).
- * Version: Version 0.3
+ * Version: Version 0.4
  * Author: Zach P <techcity@f.shmuz.in>
  * Maintainer: Oliver <oha@toktan.org>
  */
 
 function neptune_init(&$a) {
 	$a->theme_info = array();
-	$a->sourcename = 'Friendica mobile web';
+	$a->sourcename = '~f mobile web';
 	$a->videowidth = 250;
 	$a->videoheight = 200;
 	$a->theme_thread_allow = false;
